b8328a30f40ed2895a3382a5eb742bba6f7d7513
Ben Oliver <benjamin.e.oliver@gmail.com>
2020-08-27 21:09:02 +0200

    typo

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el
 ;;; beoliver 2020
 
-;;; This file serves the sole purpose of bootsrapping `init.org` on startup.
+;;; This file serves the sole purpose of bootstrapping `init.org` on startup.
 
 ;;; WARNING: if `#+PROPERTY: header-args :tangle <file>` is not set in `init.org`
 ;;; where <file> is the name of the file to be created (eg `init-extended.el`)
