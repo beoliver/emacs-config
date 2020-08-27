542eea2586839268ab2d38b86ab7990f45f7940d
Ben Oliver <benjamin.e.oliver@gmail.com>
2020-08-27 21:07:38 +0200

    cleanup

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
-
 ;;; init.el
 ;;; beoliver 2020
 
-;;; This file serves the sole purpose of bootsrapping `init.org` on startup
-;;; note that if `#+PROPERTY: header-args :tangle <some-file-name>.el` is not set in the `init.org`
+;;; This file serves the sole purpose of bootsrapping `init.org` on startup.
+
+;;; WARNING: if `#+PROPERTY: header-args :tangle <file>` is not set in `init.org`
+;;; where <file> is the name of the file to be created (eg `init-extended.el`)
 ;;; then this file will be overwritten!
 
 (require 'org)
 
-(let ((init-path (car (org-babel-tangle-file "~/.emacs.d/init.org"))))
-  (load init-path))
+(load (car (org-babel-tangle-file "~/.emacs.d/init.org")))
