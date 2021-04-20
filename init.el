890139d781c3ec646cae17083b661f89742c0cb0
Ben Oliver <benjamin.e.oliver@gmail.com>
2021-04-20 09:39:41 +0200

    use better name for generated init file

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 ;;; This file serves the sole purpose of bootstrapping `init.org` on startup.
 
 ;;; WARNING: if `#+PROPERTY: header-args :tangle <file>` is not set in `init.org`
-;;; where <file> is the name of the file to be created (eg `init-extended.el`)
+;;; where <file> is the name of the file to be created (eg `generated-init.el`)
 ;;; then this file will be overwritten!
 
 (require 'org)
